eb90971976f3584d0a33d23dd1523f00a6087242
vaisour19 <128409367+vaisour19@users.noreply.github.com>
2024-06-06 12:20:12 +0530

    first gen chat bot subject to changes

---
+++ script.js
@@ -0,0 +1,61 @@
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatInput = document.querySelector(".chat-input textarea");
+const chatbox = document.querySelector(".chatbox");
+const API_KEY = "sk-proj-BknZXb4u6cgSUbJWr9RwT3BlbkFJSWwP1HrEXd8NLEM6exXw";
+
+let userMessage;
+const createChatLi = (message, className) => {
+    const chatli = document.createElement("li");
+    chatli.classList.add("chat", className);
+    let chatContent =
+        className === "outgoing"
+            ? `<p>${message}</p>`
+            : `<span class="material-icons">smart_toy</span><p>${message}</p>`;
+    chatli.innerHTML = chatContent;
+    return chatli;
+};
+
+const generateResponse = () => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const messageElement = chatbox.querySelector(".chat.incoming p");
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${API_KEY}`,
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: userMessage }],
+        }),
+    };
+
+    fetch(API_URL, requestOptions)
+        .then((res) => res.json())
+        .then((data) => {
+            messageElement.textContent = data.choices[0].message.content;
+        })
+        .catch((error) => {
+            messageElement.textContent =
+                "Oops, something went wrong. Please try again.";
+        });
+};
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    // console.log(userMessage);
+
+    if (!userMessage) return;
+
+    // appending user message to chat box
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+
+    setTimeout(() => {
+        const incomingChatLi = createChatLi("Thinking.......", "incoming");
+        chatbox.appendChild(incomingChatLi);
+        generateResponse();
+    }, 600);
+};
+
+sendChatBtn.addEventListener("click", handleChat);
